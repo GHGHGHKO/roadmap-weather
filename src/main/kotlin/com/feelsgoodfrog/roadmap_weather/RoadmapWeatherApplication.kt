588b40efdc526cee1453d4478c46a70446f3fd2c
GHGHGHKO <gudrb963@gmail.com>
2024-10-29 22:36:47 +0900

    feat: Initial commit

---
+++ src/main/kotlin/com/feelsgoodfrog/roadmap_weather/RoadmapWeatherApplication.kt
@@ -0,0 +1,11 @@
+package com.feelsgoodfrog.roadmap_weather
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class RoadmapWeatherApplication
+
+fun main(args: Array<String>) {
+	runApplication<RoadmapWeatherApplication>(*args)
+}
---
+++ src/test/kotlin/com/feelsgoodfrog/roadmap_weather/RoadmapWeatherApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.feelsgoodfrog.roadmap_weather
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class RoadmapWeatherApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
